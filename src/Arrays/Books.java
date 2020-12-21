3f4a338e9d0d445188d3cb5f06cb3c2343ebec00
Larin Aleksei <larin_ai00@mail.ru>
2020-12-21 23:22:56 +0300

    Книги и автор

--- src/Arrays/Books.java
+++ src/Arrays/Books.java
@@ -1,4 +1,29 @@
 package Arrays;
 
 public class Books {
+    String title;
+    String author;
+
+}
+class BooksTestDrive{
+    public static void main(String[] args) {
+        Books[] myBooks = new Books[3];
+        int x = 0;
+        myBooks[0] = new Books();
+        myBooks[1] = new Books();
+        myBooks[2] = new Books();
+        myBooks[0].title = "Плоды Java";
+        myBooks[1].title = "Java Гэтсби";
+        myBooks[2].title = "Сборник рецептов по Java";
+        myBooks[0].author = "Bob";
+        myBooks[1].author = "Siy";
+        myBooks[2].author = "Jan";
+
+        while (x < 3){
+            System.out.print(myBooks[x].title);
+            System.out.print(". автор ");
+            System.out.println(myBooks[x].author);
+            x = x + 1;
+        }
+    }
 }
